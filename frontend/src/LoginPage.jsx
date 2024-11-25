8e1a2fe10f54abf0915cba2221228e13da7c3c69
yukiroow <hcdominguez07@gmail.com>
2024-11-25 21:34:26 +0800

    Add login state

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,27 +1,57 @@
 import Logo from "./assets/logo.svg";
 import BannerText from "./assets/banner-text.svg";
+import { useState } from "react";
 const LoginPage = () => {
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+    });
+
+    const handleFormInput = (event) => {
+        const key = event.target.name;
+        const value = event.target.value;
+        setCredentials((values) => ({ ...values, [key]: value }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // TODO: Binding to Server POST Request
+    };
+
     return (
         <>
-            <div className="flex flex-row h-screen bg-primary">
-                <div className="flex flex-col flex-1 mx-auto px-6 items-center rounded-tr-box rounded-br-box bg-base-100">
+            <main className="flex flex-row h-screen bg-primary">
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex flex-col flex-1 mx-auto px-6 items-center rounded-tr-box rounded-br-box bg-base-100"
+                >
                     <Banner />
                     <h1 className="text-2xl text-gray-500 font-thin self-center my-3">
                         Login to Your Account
                     </h1>
                     <input
                         type="text"
+                        name="username"
                         placeholder="Username"
                         className="input input- input-bordered w-full max-w-xs block mb-2"
+                        value={credentials["username"]}
+                        onChange={handleFormInput}
                     />
                     <input
                         type="password"
+                        name="password"
                         placeholder="Password"
                         className="input input-bordered w-full max-w-xs block mt- mb-5"
+                        value={credentials["password"]}
+                        onChange={handleFormInput}
                     />
-                    <button className="btn btn-wide block btn-secondary">Login</button>
-                </div>
-                <div className="flex flex-col mx-auto items-center px-20">
+                    <input
+                        type="submit"
+                        className="btn btn-wide block btn-secondary"
+                        value="Login"
+                    />
+                </form>
+                <aside className="flex flex-col basis-1/3 mx-auto items-center px-20">
                     <h1 className="text-3xl font-bold text-center mt-96 text-primary-content">
                         NEW HERE?
                     </h1>
@@ -29,8 +59,8 @@ const LoginPage = () => {
                         Create your Account Now
                     </p>
                     <button className="btn btn-wide mt-6">Sign-up</button>
-                </div>
-            </div>
+                </aside>
+            </main>
         </>
     );
 };
@@ -40,7 +70,11 @@ const Banner = () => {
         <>
             <div className="flex flex-row self-center mt-48 mb-14">
                 <img src={Logo} alt="Alumania Logo" className="w-36" />
-                <img src={BannerText} alt="Alumania Text name" className="w-80" />
+                <img
+                    src={BannerText}
+                    alt="Alumania Text name"
+                    className="w-80"
+                />
             </div>
         </>
     );
