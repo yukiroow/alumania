696b0730a15c7b290668d79af62e944a64576032
yukiroow <hcdominguez07@gmail.com>
2024-11-29 13:03:03 +0800

    Add AuthProvider Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,22 +8,25 @@ import ErrorPage from "./pages/ErrorPage";
 import RootLayout from "./layouts/RootLayout";
 import AuthLayout from "./layouts/AuthLayout";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthProvider } from "./hooks/useAuth";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<AuthLayout />}>
-                    <Route index element={<LoginPage />} />
-                    <Route path="signup" element={<SignupPage />} />
-                </Route>
-                <Route path="/home" element={
-                    <ProtectedRoute>
-                        <RootLayout />
-                    </ProtectedRoute>
-                } />
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<AuthLayout />}>
+                        <Route index element={<LoginPage />} />
+                        <Route path="signup" element={<SignupPage />} />
+                    </Route>
+                    <Route path="/home" element={
+                        <ProtectedRoute>
+                            <RootLayout />
+                        </ProtectedRoute>
+                    } />
+                    <Route path="*" element={<ErrorPage />} />
+                </Routes>
+            </AuthProvider>
         </BrowserRouter>
     </StrictMode>
 );
