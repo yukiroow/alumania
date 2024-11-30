d2045870345ce8eec252a5c07b8a4f84b2b4fda4
Freskkie <fresenc112233@gmail.com>
2024-11-30 13:06:12 +0800

    edit icons

--- frontend/src/components/EventsCard.jsx
+++ frontend/src/components/EventsCard.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faClock,
+    faCalendar,
+    faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
 const EventCard = () => {
     return (
         <div className="flex items-center card lg:card-side bg-base-100 shadow-xl m-40">
@@ -20,87 +25,15 @@ const EventCard = () => {
                 </p>
                 <div className="flex flex-col items-start space-y-4">
                     <div className="flex item-center space-x-2">
-                        <svg
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-5 h-5"
-                        >
-                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                            <g
-                                id="SVGRepo_tracerCarrier"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                            ></g>
-                            <g id="SVGRepo_iconCarrier">
-                                {" "}
-                                <path
-                                    d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z"
-                                    stroke="#CD0000"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                ></path>{" "}
-                                <path
-                                    d="M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z"
-                                    stroke="#CD0000"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                ></path>{" "}
-                            </g>
-                        </svg>
+                        <FontAwesomeIcon icon={faLocationDot} className="h-5 w-5 text-red-600" />
                         <p>Location Address</p>
                     </div>
                     <div className="flex item-center space-x-2">
-                        <svg
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-5 h-5"
-                        >
-                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                            <g
-                                id="SVGRepo_tracerCarrier"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                            ></g>
-                            <g id="SVGRepo_iconCarrier">
-                                {" "}
-                                <path
-                                    d="M3 9H21M7 3V5M17 3V5M6 12H8M11 12H13M16 12H18M6 15H8M11 15H13M16 15H18M6 18H8M11 18H13M16 18H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z"
-                                    stroke="#022543"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                ></path>{" "}
-                            </g>
-                        </svg>
+                        <FontAwesomeIcon icon={faCalendar} className="h-5 w-5 text-primary" />
                         <p>Date</p>
                     </div>
                     <div className="flex item-center space-x-2">
-                        <svg
-                            viewBox="0 0 24 20"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-5 h-5"
-                        >
-                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                            <g
-                                id="SVGRepo_tracerCarrier"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                            ></g>
-                            <g id="SVGRepo_iconCarrier">
-                                {" "}
-                                <path
-                                    d="M12 7V12L14.5 10.5M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                                    stroke="#022543"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                ></path>{" "}
-                            </g>
-                        </svg>
+                        <FontAwesomeIcon icon={faClock} className="h-5 w-5 text-primary" />
                         <p>Time</p>
                     </div>
                 </div>
