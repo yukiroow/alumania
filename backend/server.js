edd163b9594f4239d56b2c397a6195e84f5b18dc
yukiroow <hcdominguez07@gmail.com>
2024-11-30 11:51:38 +0800

    Change comment and job routes

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.json());
 // Route Imports
 const eventRouter = require("./routes/events");
 const userRouter = require("./routes/users");
-const experienceRouter = require("./routes/experience");
-const jobRouter = require("./routes/jobpost");
+const experienceRouter = require("./routes/experiences");
+const jobRouter = require("./routes/jobposts");
 const authRouter = require("./routes/auth");
 
 // authRouter
@@ -30,7 +30,7 @@ app.use("/experience", experienceRouter)
 app.use("/createnewexperience", experienceRouter)
 app.use("/album", experienceRouter)
 app.use("/albumexperience", experienceRouter)
-app.use("/comment", experienceRouter)
+app.use("/comment/:id", experienceRouter)
 app.use("/experienceimage", experienceRouter)
 app.use("/experiencelike", experienceRouter)
 app.use("/allexperiencelike", experienceRouter)
@@ -44,8 +44,8 @@ app.use("/editalumni", userRouter)
 app.use("/applicants", userRouter)
 
 // jobRouter
-app.use("/jobpost", jobRouter)
-app.use("/interestedinjobpost", jobRouter)
+app.use("/jobposts", jobRouter)
+app.use("/interested/:id", jobRouter)
 
 app.listen(2012, () => {
     console.log("Server started on http://localhost:2012");
