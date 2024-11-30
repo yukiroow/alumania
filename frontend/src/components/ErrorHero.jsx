4dfa4a13e476b37bd880d77d8d2b4b6b9688ac3c
yukiroow <hcdominguez07@gmail.com>
2024-11-30 11:18:36 +0800

    Add ErrorHero for Homepage components

---
+++ frontend/src/components/ErrorHero.jsx
@@ -0,0 +1,43 @@
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const ErrorHero = (props) => {
+    return (
+        <>
+            <div>
+                <section className="block m-auto w-2/5 mt-40">
+                    <FontAwesomeIcon
+                        icon={faCircleExclamation}
+                        size="10x"
+                        className="text-secondary"
+                    />
+                </section>
+                <section>
+                    <div className="block m-auto w-2/5">
+                        <h1 className="text-7xl mt-5 mb-5 text-secondary font-semibold">
+                            There was an error
+                        </h1>
+                        <p className="text-primary font-thin mb-2">
+                            Can you try refreshing the page?
+                        </p>
+                        <p className="text-primary font-thin mb-2">
+                            There might be something wrong with the magic engine
+                        </p>
+                        <p className="text-primary font-thin mb-6">
+                            Contact Alumania HQ, if you&apos;d like.
+                        </p>
+                        <button
+                            type="button"
+                            className="btn btn-outline btn-primary"
+                            onClick={() => window.location.reload(false)}
+                        >
+                            Refresh
+                        </button>
+                    </div>
+                </section>
+            </div>
+        </>
+    );
+};
+
+export default ErrorHero;
