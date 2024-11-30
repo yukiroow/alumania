61235bcef578f1e13d7294dd9b8c8f088c22786a
yukiroow <hcdominguez07@gmail.com>
2024-11-30 11:19:41 +0800

    Add Data fetching in ExperiencesPage

--- frontend/src/pages/ExperiencesPage.jsx
+++ frontend/src/pages/ExperiencesPage.jsx
@@ -1,11 +1,49 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ErrorHero from "../components/ErrorHero";
 
 const ExperiencesPage = () => {
+    const [experiences, setExperiences] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchExperiences = async () => {
+            try {
+                const res = await axios.get("http://localhost:2012/experience");
+                setExperiences(res.data);
+            } catch (error) {
+                console.log(error);
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchExperiences();
+    }, []);
+
+    if (loading) {
+        return (
+            <>
+                <div className="flex justify-center items-center h-2/3">
+                    <span className="loading loading-dots loading-lg"></span>
+                </div>
+            </>
+        );
+    }
+
+    if (error) {
+        return (
+            <>
+                <ErrorHero/>
+            </>
+        );
+    }
+
     return (
         <>
-            <main>
-                <h1>Experiences Page</h1>
-            </main>
+            <main className=""></main>
         </>
     );
 };
