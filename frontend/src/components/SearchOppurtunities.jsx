47dc1ff135c8cc3cc8e21f32b8c15723a66af411
Freskkie <fresenc112233@gmail.com>
2024-12-02 19:20:04 +0800

    hover SearchOpputurnities

--- frontend/src/components/SearchOppurtunities.jsx
+++ frontend/src/components/SearchOppurtunities.jsx
@@ -14,27 +14,11 @@ const SearchOppurtunies = () => {
         {/* Nag aauto adjust yung bg depende sa number of content)*/}
             <div className="w-2/4 bg-white rounded-2xl shadow-lg p-4 flex flex-col space-y-8">
                 {/* 1st Card*/}
-                <div className="p-10 shadow-md rounded-xl">
+                <div className="p-10 rounded-xl hover:shadow-lg cursor-pointer">
                 <div className="flex items-center gap-5">
                     <h2 className="card-title text-secondary font-bold">Job Title</h2>
                     <div className="relative flex justify-center items-center w-20 h-5 bg-accent rounded-full">
                         <p className="absolute text-text text-xs font-semibold">Hybrid</p>
-                    </div>
-                    <div className="ml-auto">
-                        <button
-                            className="flex overflow-hidden  
-                            whitespace-nowrap
-                        w-[5rem] hover:w-[6.3rem] 
-                        items-center gap-1
-                        cursor-pointer 
-                        bg-[#0059CD] 
-                        text-white px-4 py-1 rounded-full text-xs
-                        transition-all ease-in-out hover:scale 
-                        hover:scale-105 font-[revert] active:scale-100 shadow-lg"
-                        >
-                            View Job
-                            <svg fill="#ffffff" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M960 1277.853c-175.297 0-317.951-142.654-317.951-317.951 0-175.297 142.654-317.951 317.951-317.951 175.297 0 317.951 142.654 317.951 317.951 0 175.297-142.654 317.95-317.951 317.95Zm948.342-341.585C1720.645 558.648 1357.332 324 960 324c-397.333 0-760.645 234.648-948.342 612.268L0 959.902l11.658 23.634c187.697 377.62 551.01 612.268 948.342 612.268 397.333 0 760.645-234.648 948.342-612.268L1920 959.902l-11.658-23.634Z" fill-rule="evenodd"></path> </g></svg>
-                        </button>
                     </div>
                 </div>
                 <div className="flex items-center space-x-2 font-semibold text-gray-700 mt-2">
@@ -49,27 +33,11 @@ const SearchOppurtunies = () => {
                 </div>
 
                 {/* 2nd Card*/}
-                <div className="p-10 shadow-md rounded-xl">
+                <div className="p-10 rounded-xl hover:shadow-lg cursor-pointer">
                 <div className="flex items-center gap-5">
                     <h2 className="card-title text-secondary font-bold">Job Title</h2>
                     <div className="relative flex justify-center items-center w-20 h-5 bg-accent rounded-full">
                         <p className="absolute text-text text-xs font-semibold">Hybrid</p>
-                    </div>
-                    <div className="ml-auto">
-                        <button
-                            className="flex overflow-hidden  
-                            whitespace-nowrap
-                        w-[5rem] hover:w-[6.3rem] 
-                        items-center gap-1
-                        cursor-pointer 
-                        bg-[#0059CD] 
-                        text-white px-4 py-1 rounded-full text-xs
-                        transition-all ease-in-out hover:scale 
-                        hover:scale-105 font-[revert] active:scale-100 shadow-lg"
-                        >
-                            View Job
-                            <svg fill="#ffffff" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M960 1277.853c-175.297 0-317.951-142.654-317.951-317.951 0-175.297 142.654-317.951 317.951-317.951 175.297 0 317.951 142.654 317.951 317.951 0 175.297-142.654 317.95-317.951 317.95Zm948.342-341.585C1720.645 558.648 1357.332 324 960 324c-397.333 0-760.645 234.648-948.342 612.268L0 959.902l11.658 23.634c187.697 377.62 551.01 612.268 948.342 612.268 397.333 0 760.645-234.648 948.342-612.268L1920 959.902l-11.658-23.634Z" fill-rule="evenodd"></path> </g></svg>
-                        </button>
                     </div>
                 </div>
                 <div className="flex items-center space-x-2 font-semibold text-gray-700 mt-2">
