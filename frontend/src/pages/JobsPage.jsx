d59142636826a9b8cdd1fc8f1624104de1dee8b7
yukiroow <hcdominguez07@gmail.com>
2024-12-04 22:39:51 +0800

    Add interested state to jobspage

--- frontend/src/pages/JobsPage.jsx
+++ frontend/src/pages/JobsPage.jsx
@@ -3,18 +3,27 @@ import { useEffect, useState } from "react";
 import ErrorHero from "../components/ErrorHero";
 import OpportunityCard from "../components/OpportunityCard";
 import OpportunityPane from "../components/OpportunityPane";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPersonThroughWindow } from "@fortawesome/free-solid-svg-icons";
 
 const JobsPage = () => {
     const [jobs, setJobs] = useState([]);
     const [selectedJob, setSelectedJob] = useState(null);
+    const [interested, setInterested] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const userId = localStorage.getItem("userid").replace(/['"]+/g, "");
 
     useEffect(() => {
         const fetchJobs = async () => {
             try {
                 const res = await axios.get("http://localhost:2012/jobposts");
+                const resInterested = await axios.get(
+                    `http://localhost:2012/jobposts/interestedjobs/${userId}`
+                );
+                console.log(resInterested);
                 setJobs(res.data);
+                setInterested(resInterested.data);
             } catch (error) {
                 console.log(error);
                 setError(true);
@@ -27,8 +36,11 @@ const JobsPage = () => {
     }, []);
 
     const handleCardClick = (job) => {
-        setSelectedJob(job);
-    }
+        const isInterested = interested.some(
+            (interest) => interest.jobpid === job.jobpid
+        );
+        setSelectedJob({ ...job, isInterested });
+    };
 
     if (loading) {
         return (
@@ -56,12 +68,33 @@ const JobsPage = () => {
                         <OpportunityCard
                             key={job.jobpid}
                             job={job}
-                            handleCardClick={() => handleCardClick(job)}
+                            handleCardClick={handleCardClick}
                             isSelected={selectedJob?.jobpid === job.jobpid}
                         />
                     ))}
                 </section>
-                <OpportunityPane job={selectedJob} />
+                {!selectedJob ? (
+                    <div className="relative h-full overflow-hidden">
+                        <div className="card bg-white border w-[50%] h-[calc(100vh-7rem)] fixed top-24 right-10 overflow-y-auto p-4">
+                            <div className="card-body">
+                                <FontAwesomeIcon
+                                    icon={faPersonThroughWindow}
+                                    size="5x"
+                                    className="opacity-40 mt-[25%]"
+                                    color="#032543"
+                                />
+                                <h1 className="text-primary text-center opacity-40 font-thin text-2xl cursor-default select-none">
+                                    Select an Opportunity
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <OpportunityPane
+                        job={selectedJob}
+                        isInterested={selectedJob.isInterested}
+                    />
+                )}
             </main>
         </>
     );
