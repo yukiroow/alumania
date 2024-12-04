68ca39963e5719fbb3950f1ec87c9e4a61a0da81
yukiroow <hcdominguez07@gmail.com>
2024-12-04 17:14:52 +0800

    Add body parameters eto events routes

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,13 @@
 //Lapig & Maga
 const mysql = require('mysql2');
+require("dotenv").config();
 
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'alumaniadb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 });
 
 exports.db = db;
--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,15 +1,16 @@
 //Lapig & Maga
-const express = require("express")
-const router = express.Router()
-const db = require('../database').db;
+const express = require("express");
+const router = express.Router();
+const db = require("../database").db;
 
 // Get all Interested in Event
-router.get("/interestedinevents", (req, res) => {
-    db.query("SELECT * FROM interestedinevent", (err, results) => {
+router.get("/interestedinevents/:id", (req, res) => {
+    const { id } = req.params;
+    db.query("SELECT * FROM interestedinevent WHERE userid = ?", [id],(err, results) => {
         if (err) return res.status(500).json({ error: err.message });
         res.json(results);
     });
-})
+});
 
 // Get all Event
 router.get("/", (req, res) => {
@@ -17,6 +18,44 @@ router.get("/", (req, res) => {
         if (err) return res.status(500).json({ error: err.message });
         res.json(results);
     });
-})
+});
+
+// Set interested
+router.post("/interested/:id", (req, res) => {
+    const { id } = req.params;
+    const { userId } = req.body;
+    db.query(
+        "INSERT INTO interestedinevent (eventid, userid) VALUES (?, ?)",
+        [id, userId.replace(/['"]+/g, '')],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).send({
+                message: "Marked as interested",
+            });
+        }
+    );
+});
+
+// Set interested
+router.post("/disregard/:id", (req, res) => {
+    const { id } = req.params;
+    const { userId } = req.body;
+    db.query(
+        "DELETE FROM interestedinevent WHERE eventid = ? AND userid = ?",
+        [id, userId.replace(/['"]+/g, '')],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).send({
+                message: "Removed from interested!",
+            });
+        }
+    );
+});
 
 module.exports = router;
