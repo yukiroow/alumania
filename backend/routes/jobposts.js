9328aba61f6afea1de1b8027e265c11756369472
yukiroow <hcdominguez07@gmail.com>
2024-12-05 00:15:25 +0800

    Add interested and discard endpoints for jobposts

--- backend/routes/jobposts.js
+++ backend/routes/jobposts.js
@@ -12,11 +12,11 @@ router.get("/", (req, res) => {
 });
 
 // Get all Interested in Jobpost
-router.get("/interested/:id", (req, res) => {
-    const jobpostId = req.params.id;
+router.get("/interestedjobs/:id", (req, res) => {
+    const userId = req.params.id;
     db.query(
-        "SELECT * FROM interestedinjobpost as iij INNER JOIN user as al using (userid) LEFT JOIN jobpost using(jobpid) WHERE iij.jobpid = ?",
-        [jobpostId],
+        "SELECT iij.jobpid, iij.userid FROM interestedinjobpost as iij INNER JOIN user as al using (userid) LEFT JOIN jobpost using(jobpid) WHERE iij.userid = ?",
+        [userId],
         (err, results) => {
             if (err) return res.status(500).json({ error: err.message });
             if (results.length > 0) {
@@ -24,6 +24,44 @@ router.get("/interested/:id", (req, res) => {
             } else {
                 res.status(404).send("nothing");
             }
+        }
+    );
+});
+
+// Set interested
+router.post("/interested/:id", (req, res) => {
+    const { id } = req.params;
+    const { userId } = req.body;
+    db.query(
+        "INSERT INTO interestedinjobpost (jobpid, userid) VALUES (?, ?)",
+        [id, userId.replace(/['"]+/g, '')],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).send({
+                message: "Marked as interested",
+            });
+        }
+    );
+});
+
+// Set interested
+router.post("/disregard/:id", (req, res) => {
+    const { id } = req.params;
+    const { userId } = req.body;
+    db.query(
+        "DELETE FROM interestedinjobpost WHERE jobpid = ? AND userid = ?",
+        [id, userId.replace(/['"]+/g, '')],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).json({ error: err.message });
+            }
+            res.status(201).send({
+                message: "Removed from interested!",
+            });
         }
     );
 });
