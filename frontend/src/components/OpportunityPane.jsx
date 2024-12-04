1152c5f2c53f15cac1a88b90f44a59404a107d9b
yukiroow <hcdominguez07@gmail.com>
2024-12-04 22:40:39 +0800

    Add proper display of interested button

--- frontend/src/components/OpportunityPane.jsx
+++ frontend/src/components/OpportunityPane.jsx
@@ -2,35 +2,53 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faBuilding,
     faLocationDot,
-    faPersonThroughWindow,
+    faStar,
 } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 {
     /* @author Freskkie Encarnacion*/
 }
 
 const OpportunityPane = ({ job }) => {
-    if (!job) {
-        return (
-            <>
-                <div className="relative h-full overflow-hidden">
-                    <div className="card bg-white border w-[50%] h-[calc(100vh-7rem)] fixed top-24 right-10 overflow-y-auto p-4">
-                        <div className="card-body">
-                            <FontAwesomeIcon
-                                icon={faPersonThroughWindow}
-                                size="5x"
-                                className="opacity-40 mt-40"
-                                color="#032543"
-                            />
-                            <h1 className="text-primary text-center opacity-40 font-thin text-6xl cursor-default select-none">
-                                Select an Opportunity
-                            </h1>
-                        </div>
-                    </div>
-                </div>
-            </>
-        );
-    }
+    const userId = localStorage.getItem("userid");
+    const jobpId = job.jobpid;
+    const [interested, setInterested] = useState(job.isInterested);
+    useEffect(() => {
+        setInterested(job.isInterested);
+    }, [job.isInterested]);
+
+    const handleInterested = () => {
+        axios
+            .post(`http://localhost:2012/jobposts/interested/${jobpId}`, {
+                userId: `${userId}`,
+            })
+            .then((res) => {
+                if (res.status == 201) {
+                    setInterested(true);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    const handleDisregard = () => {
+        axios
+            .post(`http://localhost:2012/events/disregard/${jobpId}`, {
+                userId: `${userId}`,
+            })
+            .then((res) => {
+                if (res.status == 201) {
+                    setInterested(false);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     return (
         <>
             <div className="relative h-full overflow-hidden">
@@ -60,56 +78,49 @@ const OpportunityPane = ({ job }) => {
                                 </p>
                             </div>
                             <button
-                                className="flex overflow-hidden  
-                        w-[5rem] hover:w-[6.3rem] 
-                        items-center gap-1
-                        cursor-pointer
-                        bg-[#0059CD]
-                        text-white px-3.5 py-1 rounded-full text-xs
-                        transition-all ease-in-out hover:scale
-                        hover:scale-105 font-[revert] active:scale-100 shadow-lg"
+                                className={`btn ${
+                                    !interested
+                                        ? "btn-outline btn-secondary"
+                                        : "btn-success"
+                                } btn-xs rounded-full transform transition-all hover:scale-105`}
+                                onClick={
+                                    interested
+                                        ? handleDisregard
+                                        : handleInterested
+                                }
                             >
-                                Interested
-                                <svg
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="w-4 h-4"
-                                >
-                                    <g
-                                        id="SVGRepo_bgCarrier"
-                                        strokeWidth="0"
-                                    ></g>
-                                    <g
-                                        id="SVGRepo_tracerCarrier"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    ></g>
-                                    <g id="SVGRepo_iconCarrier">
-                                        {" "}
-                                        <path
-                                            d="M6 12H18M12 6V18"
-                                            stroke="#ffffff"
-                                            strokeWidth="2.5"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                        ></path>{" "}
-                                    </g>
-                                </svg>
+                                <FontAwesomeIcon icon={faStar} />
+                                {!interested
+                                    ? "Mark as Interested"
+                                    : "Interested"}
                             </button>
                         </div>
                         <hr className="w-[calc(100%+32px)] h-[5px] border-t border-gray-400 my-3 -ml-4 -mr-4" />
                         <h2 className="text-primary font-bold mt-2 text-xl">
                             Job Details
                         </h2>
-                        <p className="text-primary h-auto flex-grow-0">{job.description}</p>
+                        <p className="text-primary h-auto flex-grow-0">
+                            {job.description}
+                        </p>
                         <section>
                             <h2 className="text-primary font-bold mt-5 text-xl">
                                 Contact Information
                             </h2>
-                            {job.contactname && <p className="text-primary">Name: {job.contactname}</p>}
-                            {job.contactemail && <p className="text-primary">Email: {job.contactemail}</p>}
-                            {job.contactnumber && <p className="text-primary">Phone: {job.contactnumber}</p>}
+                            {job.contactname && (
+                                <p className="text-primary">
+                                    Name: {job.contactname}
+                                </p>
+                            )}
+                            {job.contactemail && (
+                                <p className="text-primary">
+                                    Email: {job.contactemail}
+                                </p>
+                            )}
+                            {job.contactnumber && (
+                                <p className="text-primary">
+                                    Phone: {job.contactnumber}
+                                </p>
+                            )}
                         </section>
                     </div>
                 </div>
