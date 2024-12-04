7f49a6808ad800952b572338dcc335a348ba92a4
yukiroow <hcdominguez07@gmail.com>
2024-12-04 17:14:31 +0800

    Add userid return for login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,12 +15,12 @@ router.get("/", (req, res) => {
 router.post("/login", (req, res) => {
     const { username, password } = req.body;
     db.query(
-        "SELECT username FROM alumni LEFT JOIN user USING (userid) WHERE username= ? AND password= ?",
+        "SELECT userid FROM alumni LEFT JOIN user USING (userid) WHERE username= ? AND password= ?",
         [username, password],
         (err, results) => {
             if (err) return res.status(500).json({ error: err.message });
             if (results.length > 0) {
-                res.status(200).send("success");
+                res.status(200).send(results[0]);
             } else {
                 res.status(403).send("Bad Credentials");
             }
