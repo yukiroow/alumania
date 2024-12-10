836b2f4f21f10824efac503765229ee8a7229c98
yukiroow <hcdominguez07@gmail.com>
2024-12-11 00:06:12 +0800

    Export LogoutModal for Sidebar

---
+++ frontend/src/components/core/LogoutModal.jsx
@@ -0,0 +1,27 @@
+const LogoutModal = (props) => {
+    return (
+        <>
+            <dialog id="logout_modal" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg text-primary">Logout</h3>
+                    <p className="py-4">Are you sure you want to logout?</p>
+                    <div className="modal-action">
+                        <button
+                            className="btn btn-warning text-white"
+                            onClick={() => {
+                                props.auth();
+                            }}
+                        >
+                            Logout
+                        </button>
+                        <form method="dialog">
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        </>
+    );
+};
+
+export default LogoutModal;
--- frontend/src/components/core/Sidebar.jsx
+++ frontend/src/components/core/Sidebar.jsx
@@ -11,6 +11,7 @@ import Logo from "../../assets/logo.svg";
 import { useAuth } from "../../hooks/useAuth";
 import { useState } from "react";
 import NewPostModal from "../post/NewPostModal";
+import LogoutModal from "./LogoutModal";
 
 {
     /* @author Freskkie Encarnacion*/
@@ -107,32 +108,6 @@ const SideBar = () => {
                     />
                 </div>
             </nav>
-        </>
-    );
-};
-
-const LogoutModal = (props) => {
-    return (
-        <>
-            <dialog id="logout_modal" className="modal">
-                <div className="modal-box">
-                    <h3 className="font-bold text-lg text-primary">Logout</h3>
-                    <p className="py-4">Are you sure you want to logout?</p>
-                    <div className="modal-action">
-                        <button
-                            className="btn btn-warning text-white"
-                            onClick={() => {
-                                props.auth();
-                            }}
-                        >
-                            Logout
-                        </button>
-                        <form method="dialog">
-                            <button className="btn">Close</button>
-                        </form>
-                    </div>
-                </div>
-            </dialog>
         </>
     );
 };
