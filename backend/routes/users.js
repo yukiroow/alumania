bec8c5c10e3d28d9be38b2906c02c5d2540ee48c
yukiroow <hcdominguez07@gmail.com>
2024-12-11 00:05:59 +0800

    Cleanup user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
 //Lapig & Maga
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const db = require("../database").db;
 
 // Get all User
@@ -11,34 +11,20 @@ router.get("/", (req, res) => {
     });
 });
 
-//Harry Dominguez
-router.post("/applicants", (req, res) => {
-    res.send("Create New Applicants")
-})
-
-// Cariel Joyce Maga
-router.put("/editalumni", (req, res) => {
-    const userid = req.params.id;
-    const { email, firstname, middlename, lastname, course, empstatus, location, company, displaypic, diploma, banned } = req.body;
-
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
     db.query(
-        'UPDATE alumni SET email = ?, firstname = ?, middlename = ?, lastname = ?, course = ?, empstatus = ?, location = ?, company = ?, displaypic = ?, diploma = ?, banned = ? WHERE userId = ?',
-        [email, firstname, middlename, lastname, course, empstatus, location, company, displaypic, diploma, banned, userid],
-        (err, result) => {
-          if (err) {
-            console.error('Error executing query: ' + err.stack);
-            return res.status(500).json({ error: 'Error updating alumni' });
-          }
-    
-          if (result.affectedRows > 0) {
-            res.status(200).json({ message: 'Alumni updated successfully' });
-          } else {
-            res.status(404).json({ error: 'Alumni not found' });
-          }
+        `SELECT userid, email, firstname, 
+      middlename, lastname, course, empstatus, 
+      location, company, displaypic, private 
+      FROM user INNER JOIN alumni USING(userid) 
+      WHERE userid = ?`,
+        [id],
+        (err, results) => {
+            if (err) return res.status(500).json({ error: err.message });
+            res.status(200).send(results[0]);
         }
     );
- });
-
-
+});
 
 module.exports = router;
