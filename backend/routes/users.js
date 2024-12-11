0b01a67cc7d3002501b421fb4d5314ea7a1ee761
yukiroow <hcdominguez07@gmail.com>
2024-12-11 23:47:11 +0800

    fix syntax error in visibility and addpfp routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -16,7 +16,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
     const { id } = req.params;
     db.query(
-        `SELECT userid, email, firstname, 
+        `SELECT userid, username, email, firstname, 
       middlename, lastname, course, empstatus, 
       location, company, displaypic, private 
       FROM user INNER JOIN alumni USING(userid) 
@@ -40,10 +40,8 @@ router.put("/setvisible/:id", (req, res) => {
         `,
         [type, id],
         (err, results) => {
-            (err, results) => {
-                if (err) return res.status(500).json({ error: err.message });
-                res.status(200).send({ message: "OK" });
-            };
+            if (err) return res.status(500).json({ error: err.message });
+            res.status(200).send({ message: "OK" });
         }
     );
 });
@@ -59,10 +57,8 @@ router.post("/uploadpfp/:id", upload.single("image"), (req, res) => {
         `,
         [pic, id],
         (err, results) => {
-            (err, results) => {
-                if (err) return res.status(500).json({ error: err.message });
-                res.status(200).send({ pfp: pic });
-            };
+            if (err) return res.status(500).json({ error: err.message });
+            res.status(200).send({ pfp: pic });
         }
     );
 });
