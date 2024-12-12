6f4cde19e9a7f74f7266016172826fa34501387f
yukiroow <hcdominguez07@gmail.com>
2024-12-12 18:29:48 +0800

    Add author tag and formatting main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+// Author @yukiroow Harry Dominguez
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -45,10 +46,7 @@ createRoot(document.getElementById("root")).render(
                             />
                         </Route>
                         <Route path="search" element={<SearchPage />} />
-                        <Route
-                            path="profile"
-                            element={<ProfilePage />}
-                        />
+                        <Route path="profile" element={<ProfilePage />} />
                     </Route>
                     <Route path="*" element={<ErrorPage />} />
                 </Routes>
