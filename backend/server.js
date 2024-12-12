1b32cb99213d5a8e9be250ef27777318ccee0bfc
yukiroow <hcdominguez07@gmail.com>
2024-12-12 17:54:54 +0800

    Cleanup server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // CORS policy
 app.use(cors());
@@ -18,41 +18,13 @@ const jobRouter = require("./routes/jobposts");
 const authRouter = require("./routes/auth");
 const searchRouter = require("./routes/search");
 
-// authRouter
+// Routes
 app.use("/auth", authRouter);
-app.use("/login", authRouter);
-
-// eventRouter
-app.use("/events", eventRouter)
-app.use("/interestedinevents", eventRouter)
-
-// experienceRouter
-app.use("/experiences", experienceRouter)
-app.use("/all/:id", experienceRouter)
-app.use("/removexperience/:id", experienceRouter)
-app.use("/new", experienceRouter)
-app.use("/likes/:id", experienceRouter)
-app.use("/isliked/:id", experienceRouter)
-app.use("/likepost/:id", experienceRouter)
-app.use("/unlikepost/:id", experienceRouter)
-
-// userRouter
-app.use("/users", userRouter)
-app.use("/alumni", userRouter)
-app.use("/editalumni", userRouter)
-app.use("/applicants", userRouter)
-
-// jobRouter
-app.use("/jobposts", jobRouter)
-app.use("/interested/:id", jobRouter)
-
-// Search
+app.use("/events", eventRouter);
+app.use("/experiences", experienceRouter);
+app.use("/users", userRouter);
+app.use("/jobposts", jobRouter);
 app.use("/search", searchRouter);
-app.use("/events/:query", searchRouter);
-app.use("/experiences/:query", searchRouter);
-app.use("/albums/:query", searchRouter);
-app.use("/users/:query", searchRouter);
-app.use("/opportunities/:query", searchRouter);
 
 app.listen(2012, () => {
     console.log("Server started on http://localhost:2012");
